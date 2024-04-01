056645cc86a66c7f1c1d1bec83fc59a79a4f00cf
Julia Wey-Burgoyne <juliawey@gmail.com>
2024-04-01 17:31:23 -0400

    add completed lab.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function saturdayFun(activity = 'roller-skate') {
-    return (`This Saturday, I want to ${activity}!`);
+function saturdayFun(activity = "roller-skate") {
+  return (`This Saturday, I want to ${activity}!`)
 }
-function mondayWork(activity = 'go to the office') {
-    return (`This Monday, I will ${activity}.`)
+
+function mondayWork(activity = "go to the office") {
+  return (`This Monday, I will ${activity}.`)
 }
+
 let wrapAdjective = function(style="*") {
     return function(adj="special") {
       return `You are ${style}${adj}${style}!`
